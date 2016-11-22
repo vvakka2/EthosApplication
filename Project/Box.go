77cdcb2364b39c037449ba1b1ba95efb45a8d320
dilip <dilip.vakkalanka@gmail.com>
2016-11-21 21:13:29 -0600

    Second Commit

--- Project/Box.go
+++ Project/Box.go
@@ -18,7 +18,10 @@ data := BoxType {1,1,3,3}
 efmt.Println("Values x1:",data.x1,"y1:",data.y1)
 efmt.Println("Second Values X2: ",data.x2," Y2: ",data.y2)
 distance := math.Sqrt(math.Pow((data.x2-data.x1),2)+math.Pow((data.y2-data.y1),2))
+newBox := BoxType {data.x1+distance,data.y1+distance,data.x2+distance,data.y2+distance}
 efmt.Println("Distance: ",distance)
+efmt.Println("Updated Values x1: ",newBox.x1," y1: ",newBox.y1)
+efmt.Println("Updated Values X2: ",newBox.x2," y2: ",newBox.y2) 
 fd,status := ethos.OpenDirectoryPath(path)
 data.Write(fd)
 efmt.Fprint(syscall.Stderr,"Success")
